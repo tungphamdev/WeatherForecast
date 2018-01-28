9dddd478fedd08bc3417a54ee9b19d4582d2e618
Tùng Phạm <tungpham@Tungs-MacBook-Pro.local>
2018-01-28 22:41:15 +0700

    fix bug

--- app/containers/WeatherPage/index.js
+++ app/containers/WeatherPage/index.js
@@ -14,17 +14,18 @@ import './weather.css';
 export class WeatherPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     const weather = this.props.weatherData;
+    if (!weather.name) return null;
     return (
       <div className="weather">
 
         <h1 className="title">{weather.name}</h1>
         <div className="line">
           <span className="line-title">Status:</span>
-          <span className="line.value">{weather.weather[0].description}</span>
+          <span className="line-value">{weather.weather[0].description}</span>
         </div>
         <div className="line">
           <span className="line-title">Temperature:</span>
-          <span className="line.value">{weather.main.temp} Kelvin</span>
+          <span className="line-value">{weather.main.temp} Kelvin</span>
         </div>
       </div>
     );
